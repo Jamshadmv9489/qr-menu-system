d462879dccc1106de8f33ab1dc461fed4c24e57d
jamshad m v <jamshadmv123@gmail.com>
2026-03-16 08:15:54 +0530

    feat(category-model): add Category schema with validation and timestamps

---
+++ server/models/Category.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+// Category Schema
+const categorySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true              // remove extra spaces
+    },
+    restaurantId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Restaurant",
+        required: true
+    }
+}, {
+    timestamps: true          // adds createdAt & updatedAt
+});
+
+export default mongoose.model("Category", categorySchema);
