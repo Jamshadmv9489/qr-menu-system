1fc68103d03a33a1c39662ff6c5436048aa53f11
jamshad m v <jamshadmv123@gmail.com>
2026-03-23 08:30:03 +0530

    add initial client setup with Vite, React, tailwindcss, react-router-dom and project configuration

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="p-10">
+      <h1 className="text-4xl font-bold text-red-500">
+        qr menu system
+      </h1>
+    </div>
+  )
+}
+
+export default App
